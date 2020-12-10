a95feab63d822ce73a4355ccdcffb84e8a0974d7
Jens Dahlkvist <Jens.Dahlkvist@sigma.se>
2020-12-10 20:43:08 +0100

    Day 6 pt2 completed

---
+++ AOC.Day6.Pt2/Program.cs
@@ -0,0 +1,46 @@
+﻿using AOC.Helpers;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC.Day5.Pt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputText = FileReader.GetProgramInput();
+            var answers = FileReader.SplitInputByNewLine(inputText).ToArray();
+
+            int yesAnswers = 0;
+            List<char[]> groupAnswers = new List<char[]>();
+            foreach (var answer in answers)
+            {
+                if (string.IsNullOrEmpty(answer))
+                {
+                    yesAnswers += UnanimousAnswers(groupAnswers);
+                    groupAnswers.Clear();
+                }
+                else
+                {
+                    groupAnswers.Add(answer.ToCharArray());
+                }
+            }
+            if(groupAnswers.Count() != 0) yesAnswers += UnanimousAnswers(groupAnswers);
+
+            Console.WriteLine($"Number of yes answers {yesAnswers}");
+            Console.ReadKey();
+        }
+        static int UnanimousAnswers(List<char[]> groupAnswers)
+        {
+            int unanimousAnswers = 0;
+            foreach (var grouping in groupAnswers.SelectMany(ga => ga).ToList().GroupBy(ans => new { ans }))
+            {
+                if (grouping.Count() == groupAnswers.Count()) unanimousAnswers++;
+            }
+            return unanimousAnswers;
+        }
+
+    }
+}

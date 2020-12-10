5aedc372c2120ec80ceb57a8b9bd7d106d6f46eb
Jens Dahlkvist <Jens.Dahlkvist@sigma.se>
2020-12-10 18:43:18 +0100

    Completed day6 pt1

---
+++ AOC.Day6.Pt1/Program.cs
@@ -0,0 +1,38 @@
+﻿using AOC.Helpers;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC.Day5.Pt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputText = FileReader.GetProgramInput();
+            var answers = FileReader.SplitInputByNewLine(inputText).ToArray();
+
+            int yesAnswers = 0;
+            List<char> groupAnswers = new List<char>();
+            foreach (var answer in answers)
+            {
+                if (string.IsNullOrEmpty(answer))
+                {
+                    yesAnswers += groupAnswers.Distinct().Count();
+                    groupAnswers.Clear();
+                }
+                else
+                {
+                    groupAnswers.AddRange(answer.ToCharArray());
+                }
+            }
+            if(groupAnswers.Count() != 0) yesAnswers += groupAnswers.Distinct().Count();
+
+            Console.WriteLine($"Number of yes answers {yesAnswers}");
+            Console.ReadKey();
+        }
+
+
+    }
+}
